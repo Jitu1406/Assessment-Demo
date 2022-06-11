e1a994ac8eb109da8e3668088542908641715d02
Jitendra Tiwari <jitendra.tiwari1406@gmail.com>
2022-06-11 17:54:36 +0530

    Initial code and added some dependencies

---
+++ src/main/java/com/assessment/breeds/breedsdemo/BreedsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.breeds.breedsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreedsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreedsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assessment/breeds/breedsdemo/BreedsDemoApplicationTests.java
@@ -0,0 +1,23 @@
+package com.assessment.breeds.breedsdemo;
+
+import com.assessment.breeds.breedsdemo.entity.Breeds;
+import com.assessment.breeds.breedsdemo.service.BreedsService;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+class BreedsDemoApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
